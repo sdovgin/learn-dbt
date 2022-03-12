3bfb2ddb5dd979c6bab5f82d7aeb9fd07508fa16
sdovgin <s1dovgin@gmail.com>
2022-03-12 16:23:23 -0500

    Adding two models and adding them to the schema.yml file

---
+++ models/example/cumulative_orders_by_date.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table') }}
+
+select distinct
+    o_orderdate,
+    sum(o_totalprice) over (order by o_orderdate) as cumulative_sales
+
+from 
+(
+  SELECT *
+  FROM snowflake_sample_data.tpch_sf1.orders
+)
+order by o_orderdate
---
+++ models/example/snowflake_customer_purchases.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }}
+
+SELECT
+    c.c_custkey,
+    c.c_name,
+    c.c_nationkey as nation,
+    sum(o.o_totalprice) as total_order_price
+FROM snowflake_sample_data.tpch_sf1.customer c
+LEFT JOIN snowflake_sample_data.tpch_sf1.orders o
+ON c.c_custkey = o.o_custkey
+group by 
+    c.c_custkey,
+    c.c_name,
+    c.c_nationkey
