b6044c64b8f5b38a57f5a56f0fc1ff6e15fed47e
sdovgin <s1dovgin@gmail.com>
2022-03-12 21:18:46 -0500

    Practicing with ephemeral models in my_first_dbt_model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,11 +1,10 @@
 
 /*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
+    Ephemeral model
 */
+
+{{ config(materialized='ephemeral') }}
+
 
 with source_data as (
 
