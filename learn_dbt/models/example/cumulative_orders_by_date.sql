141e86e50cc3ba3baa4eccb157ecf6c6846a8280
sdovgin <s1dovgin@gmail.com>
2022-03-24 01:20:49 -0400

    Limiting the data to dev environment

--- learn_dbt/models/example/cumulative_orders_by_date.sql
+++ learn_dbt/models/example/cumulative_orders_by_date.sql
@@ -8,4 +8,9 @@ select distinct
     sum(o_totalprice) over (order by o_orderdate) as cumulative_sales
 
 from orders
+
+{% if target.name == 'dev' %}
+where year(o_orderdate) = 1996
+{% endif %}
+
 order by o_orderdate
